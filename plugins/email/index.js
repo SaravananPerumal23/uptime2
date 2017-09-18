994f7993c1ca2532f31d110bbe6febda9f5c1f5a
SaravananPerumal <Saravanan.Perumal@target.com>
2017-09-18 11:12:50 -0500

    Modified email plugin-nodeJsMailer

--- plugins/email/index.js
+++ plugins/email/index.js
@@ -5,7 +5,7 @@
  *
  * Installation
  * ------------
- * This plugin is disabled by default. To enable it, add its entry 
+ * This plugin is disabled by default. To enable it, add its entry
  * to the `plugins` key of the configuration:
  *
  *   // in config/production.yaml
@@ -14,7 +14,7 @@
  *
  * Usage
  * -----
- * This plugin sends an email each time a check is started, goes down, or goes back up. 
+ * This plugin sends an email each time a check is started, goes down, or goes back up.
  * When the check goes down, the email contains the error details:
  *
  *   Object: [Down] Check "FooBar" just went down
@@ -36,7 +36,7 @@
  *     method:      SMTP  # possible methods are SMTP, SES, or Sendmail
  *     transport:         # see https://github.com/andris9/nodemailer for transport options
  *       service:   Gmail
- *       auth:            
+ *       auth:
  *         user:    foobar@gmail.com
  *         pass:    gursikso
  *     event:
@@ -44,20 +44,27 @@
  *       down:      true
  *       paused:    false
  *       restarted: false
- *     message:           
+ *     message:
  *       from:     'Fred Foo <foo@blurdybloop.com>'
  *       to:       'bar@blurdybloop.com, baz@blurdybloop.com'
  *     # The email plugin also uses the main `url` param for hyperlinks in the sent emails
  */
 var fs         = require('fs');
 var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 var moment     = require('moment');
 var CheckEvent = require('../../models/checkEvent');
 var ejs        = require('ejs');
 
 exports.initWebApp = function(options) {
   var config = options.config.email;
-  var mailer = nodemailer.createTransport(config.method, config.transport);
+  //var mailer = nodemailer.createTransport(config.method, config.transport);
+
+  var mailer = nodemailer.createTransport(smtpTransport({
+    host: config.host,
+    port: config.port
+  }));
+
   var templateDir = __dirname + '/views/';
   CheckEvent.on('afterInsert', function(checkEvent) {
     if (!config.event[checkEvent.message]) return;
